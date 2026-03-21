423b4e2f84d7f3d0e70832010dc94da714976ff8
Anton <artemtarko78@gmail.com>
2026-03-21 20:25:18 +0200

    lab

---
+++ lab_12/script.js
@@ -0,0 +1,25 @@
+// Завдання 2
+console.log("Жеруха"); 
+
+// Завдання 3
+let firstVar = "Початкове значення";
+let secondVar = 100;
+
+alert("Змінна 1: " + firstVar);
+alert("Змінна 2: " + secondVar);
+
+secondVar = firstVar;
+
+alert("Змінна 1 (після копіювання): " + firstVar);
+alert("Змінна 2 (після копіювання): " + secondVar);
+
+// Завдання 4
+const dataTypesObject = {
+    "String": "Hello world!",
+    "Number": 42,
+    "Boolean": true,
+    "Undefined": undefined,
+    "Null": null
+};
+
+console.log(dataTypesObject);
